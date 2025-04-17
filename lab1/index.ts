c9720374dc1763b631b1bdbe4fdae52c5e573785
anya647 <phlantnerd001@gmail.com>
2025-04-17 21:46:19 +0400

    Add files via upload

---
+++ lab1/index.ts
@@ -0,0 +1,13 @@
+function loadData() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'data.json', false);
+    xhr.send();
+    if (xhr.status !== 200) {
+        alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+
+    } else {
+        alert(xhr.responseText);
+    }
+}
+
+loadData();
